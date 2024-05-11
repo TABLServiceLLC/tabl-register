e8deb73f0abb12cf7001f8937994d08acb3c7024
Shayne Stark <shaynesstark@gmail.com>
2024-05-10 22:54:40 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+document.getElementById('registrationForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+
+    fetch('https://backend.tabl.page/emails/register/', {
+        method: 'POST',
+        body: formData,
+    })
+        .then((response) => response.json())
+        .then((data) => {
+            this.reset();
+            alert('Registration successful! Please allow up to 48 hours for account creation.');
+        })
+        .catch((error) => console.error('Error:', error));
+});
